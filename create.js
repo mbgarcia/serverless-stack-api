6d25c818128b2d2cb2b3dae1287011686b372de9
Marcelo Garcia <marcelogarcia@Marcelos-MacBook-Pro.local>
2020-04-05 16:34:13 -0300

    refactoring dynamodb call

--- create.js
+++ create.js
@@ -1,11 +1,11 @@
 import uuid from "uuid";
 import AWS from "aws-sdk";
+import * as dynamoDbLib from './libs/dynamodb-libs';
+import { success, failure } from './libs/response-lib';
 
 AWS.config.update({region: 'sa-east-1'});
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -28,30 +28,11 @@ export function main(event, context, callback) {
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+  try {
+    await dynamoDbLib.call('put', params);
+    return success(params.Item);
+  } catch (error) {
+    console.log(error);
+    return failure({status: false});
+  }
 }
